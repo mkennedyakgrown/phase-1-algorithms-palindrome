208250b9210767adf00ea00771dd24a8185ec1e2
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-07-24 10:57:27 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let drow = '';
+  for (let i = word.length - 1; i >= 0; i--) {
+    drow = drow + word[i];
+  };
+  revWord = drow.toString();
+  if (word == drow) {
+    return true;
+  } else {
+    return false;
+  };
 }
 
 /* 
   Add your pseudocode here
+  declare a string variable
+  iterate over 'word' in reverse order, building new variable backwards
+  compare 'word' to new variable: if same, true; if not same, false
 */
 
 /*
@@ -20,6 +33,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("question"));
+
+  
 }
 
 module.exports = isPalindrome;
